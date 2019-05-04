42f16024c26af5b4bb5b9c7fc92a04850ed156b0
marceli99 <marceli99@protonmail.com>
2019-05-04 21:33:38 +0200

    Seperated download data from controller to service

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
-
+use App\Service\DataService;
 class MainController extends AbstractController
 {
     /**
@@ -13,33 +13,9 @@ class MainController extends AbstractController
      */
     public function index()
     {
-      $json = json_decode(file_get_contents("https://api.coindesk.com/v1/bpi/currentprice.json"), true); # DEVELOPMENT ONLY
-      $exchange = json_decode(file_get_contents("https://api.exchangeratesapi.io/latest?base=USD"), true); # DEVELOPMENT ONLY
-      $today = date("Y-m-d");
-      $history = json_decode(file_get_contents("https://api.coindesk.com/v1/bpi/historical/close.json?start=2010-07-17&end={$today}"), true);
-      $chart = [
-        $history['bpi'][date ( 'Y-m-d' , strtotime ( '-6 year' , strtotime ( $today ) ) )],
-        $history['bpi'][date ( 'Y-m-d' , strtotime ( '-5 year' , strtotime ( $today ) ) )],
-        $history['bpi'][date ( 'Y-m-d' , strtotime ( '-4 year' , strtotime ( $today ) ) )],
-        $history['bpi'][date ( 'Y-m-d' , strtotime ( '-3 year' , strtotime ( $today ) ) )],
-        $history['bpi'][date ( 'Y-m-d' , strtotime ( '-2 year' , strtotime ( $today ) ) )],
-        $history['bpi']['2017-12-16'],
-        $history['bpi'][date ( 'Y-m-d' , strtotime ( '-1 year' , strtotime ( $today ) ) )],
-        $history['bpi'][date ( 'Y-m-d' , strtotime ( '-6 month' , strtotime ( $today ) ) )],
-        $history['bpi'][date ( 'Y-m-d' , strtotime ( '-3 month' , strtotime ( $today ) ) )],
-        $history['bpi'][date ( 'Y-m-d' , strtotime ( '-2 month' , strtotime ( $today ) ) )],
-        $history['bpi'][date ( 'Y-m-d' , strtotime ( '-1 month' , strtotime ( $today ) ) )],
-        $history['bpi'][date ( 'Y-m-d' , strtotime ( '-3 week' , strtotime ( $today ) ) )],
-        $history['bpi'][date ( 'Y-m-d' , strtotime ( '-2 week' , strtotime ( $today ) ) )],
-        $history['bpi'][date ( 'Y-m-d' , strtotime ( '-1 week' , strtotime ( $today ) ) )],
-        $history['bpi'][date ( 'Y-m-d' , strtotime ( '-3 day' , strtotime ( $today ) ) )],
-        $history['bpi'][date ( 'Y-m-d' , strtotime ( '-2 day' , strtotime ( $today ) ) )],
-        $history['bpi'][date ( 'Y-m-d' , strtotime ( '-1 day' , strtotime ( $today ) ) )],
-        $json['bpi']['USD']['rate_float']
-    ];
-      $history = $history['bpi']['2019-05-01'];
+      $DataService = new DataService();
         return $this->render('index.html.twig', [
-            'chart' => $chart,
+            'chart' => $DataService->CreateChart(),
         ]);
     }
     /**
@@ -47,28 +23,7 @@ class MainController extends AbstractController
      */
     public function data()
     {
-      $exchange = json_decode(file_get_contents("https://api.exchangeratesapi.io/latest?base=USD"), true); # DEVELOPMENT ONLY
-      $json = json_decode(file_get_contents("https://api.coindesk.com/v1/bpi/currentprice.json"), true); # DEVELOPMENT ONLY
-      $today = date("Y-m-d");
-      $history = json_decode(file_get_contents("https://api.coindesk.com/v1/bpi/historical/close.json?start=2010-07-17&end={$today}"), true);
-      $yesterday = strtotime ( '-1 day' , strtotime ( $today ) ) ;
-      $yesterday = date ( 'Y-m-d' , $yesterday );
-      $weekago = strtotime ( '-1 week' , strtotime ( $today ) ) ;
-      $weekago = date ( 'Y-m-d' , $weekago );
-      $monthago = strtotime ( '-1 month' , strtotime ( $today ) ) ;
-      $monthago = date ( 'Y-m-d' , $monthago );
-      $json = [
-      'usd' => round($json['bpi']['USD']['rate_float'], 0),
-      'eur' => round($json['bpi']['USD']['rate_float'] * $exchange['rates']['EUR'], 0),
-      'pln' => round($json['bpi']['USD']['rate_float'] * $exchange['rates']['PLN'], 0),
-      'gbp' => round($json['bpi']['USD']['rate_float'] * $exchange['rates']['GBP'], 0),
-      'jpy' => round($json['bpi']['USD']['rate_float'] * $exchange['rates']['JPY'], 0),
-      'chf' => round($json['bpi']['USD']['rate_float'] * $exchange['rates']['CHF'], 0),
-      'yesterday' => round((($json['bpi']['USD']['rate_float'] / $history['bpi'][$yesterday]) - 1) * 100, 2),
-      'weekago' => round((($json['bpi']['USD']['rate_float'] / $history['bpi'][$weekago]) - 1) * 100, 2),
-      'monthago' => round((($json['bpi']['USD']['rate_float'] / $history['bpi'][$monthago]) - 1) * 100, 2)
-
-    ];
-        return new JsonResponse($json);
+        $DataService = new DataService();
+        return new JsonResponse($DataService->DownloadData());
     }
 }
