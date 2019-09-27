44abe104187f78948c77dea624b54a1f01750cd9
Marceli99 <marceli99@protonmail.com>
2019-09-27 22:05:55 +0200

    php_cs_fixer fixes

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,10 +3,10 @@
 namespace App\Controller;
 
 use App\Service\DataService;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
